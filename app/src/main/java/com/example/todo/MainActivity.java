3e0c6c15973863d5e14a4db86787b6f1d318cc2d
Amindu Liyanaratne <reservedchery@gmail.com>
2024-07-23 17:24:41 +0530

    fixed app crashing due to null exception
    fixed app crash when executing both splash and main activity due getsupportactionbar returns null

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -10,22 +10,52 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.todo.Adapter.TodoAdapter;
+import com.example.todo.Model.TodoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView taskRecyclerView;
+    private TodoAdapter taskAdapter;
+    private List<TodoModel> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        getSupportActionBar().hide();
+        if(getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+
         taskRecyclerView = findViewById(R.id.taskRecyclerview);
         taskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskList = new ArrayList<>();
+
+        taskAdapter = new TodoAdapter(this);
+        taskRecyclerView.setAdapter(taskAdapter);
+
+        TodoModel task = new TodoModel();
+        task.setContent("this is a test task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        taskAdapter.setTasks(taskList);
     }
 }
--- app/src/main/java/com/example/todo/SplashActivity.java
+++ app/src/main/java/com/example/todo/SplashActivity.java
@@ -10,6 +10,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Objects;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -17,12 +19,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        getSupportActionBar().hide();
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
 
         final Intent i = new Intent(SplashActivity.this, MainActivity.class);
         new Handler().postDelayed(new Runnable() {
